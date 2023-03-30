46c5ab24b29559417fc52963217fb2604ec48409
Andrew Vickerman <andrew@Andrew-2020-MacBook-Pro.local>
2023-03-29 23:44:14 -0400

    Fixes included dependencies

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-export { OSlider } from './scripts/components/OSlider'
-export { OSliderNext } from './scripts/components/OSliderNext'
-export { OSliderPrev } from './scripts/components/OSliderPrev'
-export { OSliderPage } from './scripts/components/OSliderPage'
-export { OSliderAutoplayControls } from './scripts/components/OSliderAutoplayControls'
-export { OSliderPaginationText } from './scripts/components/OSliderPaginationText'
+import { OSlider } from './scripts/components/OSlider'
+import { OSliderNext } from './scripts/components/OSliderNext'
+import { OSliderPrev } from './scripts/components/OSliderPrev'
+import { OSliderPage } from './scripts/components/OSliderPage'
+import { OSliderAutoplayControls } from './scripts/components/OSliderAutoplayControls'
+import { OSliderPaginationText } from './scripts/components/OSliderPaginationText'
+
+export {
+  OSlider,
+  OSliderNext,
+  OSliderPrev,
+  OSliderPage,
+  OSliderAutoplayControls,
+  OSliderPaginationText
+}
