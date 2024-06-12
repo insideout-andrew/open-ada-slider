5206384e5d9927dc00967ce2001991820942642e
Andrew Vickerman <andrew@Andrew-2020-MacBook-Pro.local>
2024-06-12 09:59:42 -0400

    Adds format to pagination

--- scripts/components/OSliderPaginationText.js
+++ scripts/components/OSliderPaginationText.js
@@ -26,7 +26,12 @@ class OSliderPaginationText extends HTMLElement {
   _init(){
     const activePage = parseInt(this._slider.currentPage) + 1
     const totalPages = this._slider.totalPages
-    this.textContent = totalPages > 1 ? `Page ${activePage} of ${totalPages}` : ``    
+    if(this.hasAttribute('format')){
+      let format = this.getAttribute('format').replace("{{x}}", activePage).replace("{{y}}", totalPages)
+      this.textContent = totalPages > 1 ? format : ``
+    } else {
+      this.textContent = totalPages > 1 ? `Page ${activePage} of ${totalPages}` : ``
+    }
   }
 }
 
